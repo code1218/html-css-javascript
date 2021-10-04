78f17724d830c154e7e96a700bb2ccaa67869639
junil <skjil1218@gmail.com>
2021-10-04 17:03:33 +0900

    17:03 commit

---
+++ kakao/js/sign_in.js
@@ -0,0 +1,22 @@
+const item_ip = document.querySelectorAll('.item_ip');
+item_ip[0].onclick = () => {
+    const info_tip = document.querySelector('.info_tip');
+    info_tip.style.display = 'block';
+}
+
+item_ip[0].onkeyup = () => {
+    const util_tf = document.querySelector('.util_tf');
+    util_tf.style.display = 'block';
+    if(item_ip[0].value.length < 1){
+        util_tf.style.display = 'none';
+    }
+}
+
+item_ip[0].onblur = () => {
+    const info_tip = document.querySelector('.info_tip');
+    if(item_ip[0].value.length == 0){
+        const util_tf = document.querySelector('.util_tf');
+        info_tip.style.display = 'none';
+        util_tf.style.display = 'none';
+    }
+}
